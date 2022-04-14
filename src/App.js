4ad2bb8736331a42e7f56409def1168cd1b9fc89
shtabdeepaull <shtabdee30@gmail.com>
2022-04-14 20:11:37 +0600

    added register form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './Pages/Shared/Footer/Footer';
 import ServiceDetail from './Pages/ServiceDetails/ServiceDetails';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
         <Route path="/service/:serviceId" element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
-        |<Route path="*" element={<NotFound></NotFound>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
