fba061c61554b9b4dac00b57127f0014ea88b662
shtabdeepaull <shtabdee30@gmail.com>
2022-04-14 20:36:10 +0600

    set env variable for firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA2XchYeiT6TbU5CipIM3p6mGghJRN37M0",
-  authDomain: "genius-car-services-994d0.firebaseapp.com",
-  projectId: "genius-car-services-994d0",
-  storageBucket: "genius-car-services-994d0.appspot.com",
-  messagingSenderId: "380464534577",
-  appId: "1:380464534577:web:d97458709c44d595a97110"
+  apiKey:process.env.REACT_APP_apiKey, 
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId, 
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId, 
 };
 
 // Initialize Firebase
